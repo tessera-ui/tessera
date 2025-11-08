d0179afc62df5f9bcd2280b22a3abc7c5b100213
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2025-11-08 14:10:33 +0800

    refactor(main): clean up unused imports and enhance logging setup

--- cargo-tessera/templates/blank/src/main.rs
+++ cargo-tessera/templates/blank/src/main.rs
@@ -1,8 +1,4 @@
-use tessera_ui::{Color, Dp, Renderer, tessera};
-use tessera_ui_basic_components::{
-    surface::{surface, SurfaceArgs},
-    text::{text, TextArgsBuilder},
-};
+use tessera_ui::{Renderer, tessera};
 
 #[tessera]
 fn app() {
@@ -10,6 +6,15 @@ fn app() {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .or_else(|_| tracing_subscriber::EnvFilter::try_new("off,tessera_ui=info"))
+        .unwrap();
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_env_filter(filter)
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
+        .init();
+
     Renderer::run(app, |app| {
         tessera_ui_basic_components::pipelines::register_pipelines(app);
     })?;
