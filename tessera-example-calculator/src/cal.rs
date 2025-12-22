4e6e8e3aa8e7c144263140f75dd116ded0323872
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2025-12-22 15:35:18 +0800

    chore: remove calculator example

--- tessera-example-calculator/src/cal.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::anyhow;
-use rsc::{Interpreter, parse, tokenize};
-
-pub fn evaluate(input: &str, interpreter: &mut Interpreter<f64>) -> anyhow::Result<f64> {
-    let tokens = tokenize(input).map_err(|e| anyhow!("{:?}", e))?;
-    let expr = parse(&tokens).map_err(|e| anyhow!("{:?}", e))?;
-    let result = interpreter.eval(&expr).map_err(|e| anyhow!("{:?}", e))?;
-    Ok(result)
-}
