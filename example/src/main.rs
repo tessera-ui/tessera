c64633d309a0dee83cae73896b72aad23b495d85
shadow3aaa <74781933+shadow3aaa@users.noreply.github.com>
2026-02-03 21:38:36 +0800

    chore(templates): drop android cfg from desktop main

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[cfg(not(target_os = "android"))]
 fn main() {
     if let Err(err) = example_lib::run().run_desktop() {
         eprintln!("App failed to run: {err}");
